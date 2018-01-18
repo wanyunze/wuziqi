bbab93f956fd898391bbcca31ab27b90ad8590c9
Yunze Wan <u6404610@anu.edu.au>
2018-01-17 18:46:23 -0800

    Structural

--- src/ValidPieces.java
+++ src/ValidPieces.java
@@ -4,8 +4,15 @@
 public class ValidPieces {
     // 棋盘格子以 "A1" "B3"这种方式表明， 如果有多个棋子把它变成一个String “A1B3G3"如此。
     // 棋子必须得在棋盘上 并且不能有重复的
-    static boolean isPlacementWellFormed(String placements) {
-        
+
+    //单一棋子
+    static boolean isPlacementWellFormed(String placement){
+        return false;
+    }
+
+    //多个旗子
+    static boolean isPlacementsWellFormed(String placements) {
+        return false;
     }
 
     private static boolean helperDuplicates(String placements) {
