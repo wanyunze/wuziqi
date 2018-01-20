6c83cead7ba3b1c5400fb17751f22cf0accb018d
Yunze Wan <u6404610@anu.edu.au>
2018-01-20 05:19:13 -0800

    Structural

--- src/ValidPieces.java
+++ src/ValidPieces.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by 万运泽 on 2018/1/17.
  */
@@ -5,13 +8,64 @@ public class ValidPieces {
     // 棋盘格子以 "A1" "B3"这种方式表明， 如果有多个棋子把它变成一个String “A1B3G3"如此。
     // 棋子必须得在棋盘上 并且不能有重复的
 
-    //单一棋子
+    //单一棋子function
     static boolean isPlacementWellFormed(String placement){
-        return false;
+        char[] placementArray = placement.toCharArray();
+        char validLetter = 'A';
+        char validNumber = '1';
+        char validNumberInt = 10;
+        boolean letterCheck = false;
+        boolean numberCheck = false;
+        // Check if letter it's in range between 'A' - 'O'
+        while (validLetter <= 'O') {
+            if (placementArray[0] == validLetter) {
+                letterCheck = true;
+                break;
+            }else{
+                validLetter++;
+            }
+        }
+        // check if the length is between 2-3, if it's only length of 2. check if it's between 1-9;
+        if (placementArray.length == 2) {
+            while (validNumber <= '9') {
+                if (placementArray[1] == validNumber) {
+                    numberCheck = true;
+                    break;
+                } else {
+                    validNumber++;
+                }
+            }
+        }
+        // if it's length of 3. convert the string to number and check if it's between 10-15
+        else if (placementArray.length == 3){
+           String numberString = placementArray[1] + "" +placementArray[2];
+           // if It's not number, throw exception to avoid bugs
+           try {
+               Integer.parseInt(numberString);
+           }catch(NumberFormatException e){
+               return false;
+           }
+            int number = Integer.parseInt(numberString);
+           while (validNumberInt <= 15){
+               if (number == validNumberInt){
+                   numberCheck = true;
+                   break;
+               }else{
+                   validNumberInt++;
+               }
+           }
+        }
+        // if the length is incorrect, return false
+        else{
+            return false;
+        }
+
+        return (letterCheck && numberCheck);
     }
 
-    //多个旗子
+    //多个棋子 傻逼黎快做 做不出拖出去举报
     static boolean isPlacementsWellFormed(String placements) {
+
         return false;
     }
 
